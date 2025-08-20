c710e899e5901433c22bdfe0749cc14365a38747
Sri <srilakshmi.cr3@gmail.com>
2025-08-21 01:18:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DataArt/', // This should match your repository name
+  base: '/DataArt/', // Must match your GitHub repo name exactly
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
