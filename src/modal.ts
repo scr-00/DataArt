87ea6fd3c7bfb8e26cf6cb3c83de5dc040eaa9cb
Sri <srilakshmi.cr3@gmail.com>
2026-04-17 12:42:40 +0530

    Update modal.ts

--- src/modal.ts
+++ src/modal.ts
@@ -1,1 +1,14 @@
+import { Event } from "./types";
 
+export function openModal(event: Event) {
+  const modal = document.getElementById("modal");
+  const body = document.getElementById("modal-body");
+
+  if (modal && body) {
+    modal.classList.remove("hidden");
+    body.innerHTML = `
+      <h2>${event.title}</h2>
+      <p>${event.description}</p>
+    `;
+  }
+}
