67ea2fc9bb1dee81324dda904fe99b0972643139
Sri <srilakshmi.cr3@gmail.com>
2026-04-17 12:42:21 +0530

    Update renderer.ts

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,1 +1,12 @@
+import { Event } from "./types";
 
+export function renderEvents(events: Event[]) {
+  const timeline = document.getElementById("timeline");
+
+  events.forEach(event => {
+    const div = document.createElement("div");
+    div.className = "event";
+    div.innerText = `${event.year} - ${event.title}`;
+    timeline?.appendChild(div);
+  });
+}
