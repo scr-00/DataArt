8d49287b0653afe34988c76b2d46d1f9a7bbb702
Sri <srilakshmi.cr3@gmail.com>
2026-04-17 12:42:55 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { fetchEvents } from "./fetcher";
+import { renderEvents } from "./renderer";
 
+async function init() {
+  const events = await fetchEvents();
+  renderEvents(events);
+}
+
+init();
