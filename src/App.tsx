9b3d0ff0798152eef9ee60a95612663974fa7a85
Sri <srilakshmi.cr3@gmail.com>
2026-04-17 12:48:22 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Timeline from "./components/Timeline";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Timeline />
+    </>
+  );
+}
